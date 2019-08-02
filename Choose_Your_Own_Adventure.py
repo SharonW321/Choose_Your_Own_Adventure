2ee06b2db004ab858d475b0f9bbdf4332e7a4ba1
SharonW321 <52180616+SharonW321@users.noreply.github.com>
2019-08-02 10:19:44 -0700

    Add files via upload

---
+++ Choose_Your_Own_Adventure.py
@@ -0,0 +1,32 @@
+# Update this text to match your story.
+start = '''
+You wake up one morning and find that you aren't in your bed; you aren't even in your room.
+You're in the middle of a giant maze.
+A sign is hanging from the ivy: "You have one hour. Don't touch the walls."
+There is a hallway to your right and to your left.
+'''
+
+print(start)
+
+print("Type 'left' to go left or 'right' to go right.") # Update to match your story.
+user_input = input()
+if user_input == "left":
+    print("You decide to go left and...")
+    print("*Gasp* Fall into a ditch and bust your leg open :0")
+    print("You start limping and find that it's easier to roll.")
+    print("You struggle to your feet to find a spork in the road. ;)")
+    print("Will you take your sad self to the left or to the right?")
+    if user_input == "left":
+        print("You continue on your way and find a door in the wall.")
+        print("Will you open the door or continue on the path?")#Update to match your story.
+elif user_input == "right":
+    print("You choose to go right and...")
+    print("Step onto a cloud that takes you upwards! :)")
+    print("You continue on your path and spot a divergence in the road")
+    print("Do you go left or right?")
+    if user_input == "left"
+        print("You turn to the left see a door.")
+        print("You notice that you can continue on your path or go through the door hiding in the wall.")
+    elif user_input == "right"
+        print("You continue walking and soon discover a ledge.")
+        print("Do you stand on the ledge of")
